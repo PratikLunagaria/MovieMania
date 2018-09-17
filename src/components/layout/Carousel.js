f0414fb90ec25cc655558d42eda1c8aa7a154372
Pratik Lunagaria <pratiklunagaria@hotmail.com>
2018-09-17 20:06:42 +0530

    fixed images and dynamic routes

--- src/components/layout/Carousel.js
+++ src/components/layout/Carousel.js
@@ -8,25 +8,25 @@ class BgCarousel extends Component {
                     <div className="carousel-item has-background is-active" style={{zIndex : 1, order: 1, opacity: 0.3}}>
                         <img
                         className="is-background"
-                        src="static/one.jpg"
+                        src="/static/one.jpg"
                         alt="First" width="640" height="310" draggable="false"/>
                     </div>
                     <div className="carousel-item has-background" style={{zIndex: 0, order: 2, opacity: 0.3}}>
                         <img
                         className="is-background"
-                        src="static/two.jpg"
+                        src="/static/two.jpg"
                         alt="Second" width="640" height="310" draggable="false"/>
                     </div>
                     <div className="carousel-item has-background" style={{zIndex: 0, order: 3, opacity: 0.3}}>
                         <img
                         className="is-background"
-                        src="static/three.jpg"
+                        src="/static/three.jpg"
                         alt="Third" width="640" height="310" draggable="false"/>
                     </div>
                     <div className="carousel-item has-background" style={{zIndex: 0, order: 0, opacity: 0.3}}>
                         <img
                         className="is-background"
-                        src="static/four.jpg"
+                        src="/static/four.jpg"
                         alt="Fourth" width="640" height="310" draggable="false"/>
                     </div>
                 </div>
